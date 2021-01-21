60119b964b759175d566cf0ed4a45636996d50e7
Obmaikano <obmakno@gmail.com>
2021-01-21 03:30:48 +0200

    First commit.

---
+++ simple-notes/src/main/java/com/obmaikano/simplenotes/SimpleNotesApplication.java
@@ -0,0 +1,13 @@
+package com.obmaikano.simplenotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleNotesApplication.class, args);
+	}
+
+}
---
+++ simple-notes/src/test/java/com/obmaikano/simplenotes/SimpleNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obmaikano.simplenotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
